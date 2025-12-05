ad1eecc602d8f79194c12c86729c0304cc915faf
Amey Tripathi (k23163890) <k23163890@nmslab-004087.nms.kcl.ac.uk>
2025-12-05 13:34:29 +0000

    delivery client

--- delivery_robot/scripts/delivery_client.py
+++ delivery_robot/scripts/delivery_client.py
@@ -4,10 +4,33 @@ import actionlib
 from geometry_msgs.msg import Point
 from delivery_robot.msg import GoToLocationAction, GoToLocationGoal, LookAtAction, LookAtGoal
 
+
+
+def go_to_kitchen(goto_client):
+    rospy.loginfo("Going to KITCHEN...")
+    goto_client.send_goal(GoToLocationGoal(location_name="kitchen"))
+    goto_client.wait_for_result()
+    rospy.loginfo("IN KITCHEN")
+
+
+def look_at_person(look_client):
+    rospy.loginfo("Looking at PERSON...")
+    person = Point(x=3.0, y=1.0, z=0.0)
+    look_client.send_goal(LookAtGoal(target=person))
+    look_client.wait_for_result()
+    rospy.loginfo("Looked at Person")
+
+
+def go_to_living_room(goto_client):
+    rospy.loginfo("Going to LIVING ROOM...")
+    goto_client.send_goal(GoToLocationGoal(location_name="living_room"))
+    goto_client.wait_for_result()
+
+
 def main():
     rospy.init_node('delivery_client')
 
-    # Create action clients
+    rospy.loginfo("Creating action clients")
     goto_client = actionlib.SimpleActionClient('goto_location', GoToLocationAction)
     look_client = actionlib.SimpleActionClient('look_at', LookAtAction)
 
@@ -16,24 +39,11 @@ def main():
     look_client.wait_for_server()
     rospy.loginfo("Connected to action servers.")
 
-    #Kichen
-    rospy.loginfo("Going to KITCHEN...")
-    goto_client.send_goal(GoToLocationGoal(location_name="kitchen"))
-    goto_client.wait_for_result()
-    rospy.loginfo("IN KITCHEN")
+    go_to_kitchen(goto_client)
 
+    look_at_person(look_client)
 
-    #Person
-    rospy.loginfo("Looking at PERSON...")
-    person = Point(x=3.0, y=1.0, z=0.0)
-    look_client.send_goal(LookAtGoal(target=person))
-    look_client.wait_for_result()
-    rospy.loginfo("Looked at Person")
-
-    #Living room
-    rospy.loginfo("Going to LIVING ROOM...")
-    goto_client.send_goal(GoToLocationGoal(location_name="living_room"))
-    goto_client.wait_for_result()
+    go_to_living_room(goto_client)
 
     rospy.loginfo("Delivery task complete!")
 
